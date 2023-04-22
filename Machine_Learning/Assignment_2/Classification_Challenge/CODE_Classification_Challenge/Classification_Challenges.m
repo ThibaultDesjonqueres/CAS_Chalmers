4f992e07eccfda1f18071f7e7fc5d6c449f74a0a
ThibaultDesjonqueres <45844008+ThibaultDesjonqueres@users.noreply.github.com>
2023-04-22 10:09:58 -0200

    Add files via upload
    This file is messy.

---
+++ Machine_Learning/Assignment_2/Classification_Challenge/CODE_Classification_Challenge/Classification_Challenges.m
@@ -0,0 +1,51 @@
+
+%%%%%%%%%%%% Classification_Challenge_Code %%%%%%%%%
+
+clear all;
+
+xTest2 = loadmnist2();
+[xTrain, tTrain, xValid, tValid, xTest, tTest] = LoadMNIST(3);
+
+
+%%
+layers = [
+    imageInputLayer([28 28 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+%Play With the parameters. 30 epochs recommended but 5 works fine.
+
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs',5, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{xValid,tValid}, ...
+    'ValidationFrequency',30, ...
+    'ValidationPatience',5,...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+
+
+net = trainNetwork(xTrain,tTrain,layers,options);
+%%
+YPred = classify(net,xTest2);
+writematrix(YPred,"classifications.csv");
